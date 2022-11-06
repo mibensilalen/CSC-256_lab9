dcdd69f99a7734d64b19e0e232e06300db12629a
mibensilalen <114273347+mibensilalen@users.noreply.github.com>
2022-11-06 16:34:14 -0500

    Update ddg.py

--- ddg.py
+++ ddg.py
@@ -1,5 +1,5 @@
 import requests
-url_ddg = 'https://api.duckduckgo.com/?q=presidents+of+the+united+states&format=json'
+url= 'https://api.duckduckgo.com/?q=presidents+of+the+united+states&format=json'
 
 resp_data = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
 
